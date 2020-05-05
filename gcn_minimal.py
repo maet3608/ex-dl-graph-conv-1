53cbdddf928fa237159c57147b47f7a5b469a634
Stefan Maetschke <stefan.maetschke@gmail.com>
2020-05-05 15:44:44 +1000

    minor refinements

--- gcn_minimal.py
+++ gcn_minimal.py
@@ -2,12 +2,17 @@ import dgl
 
 import torch as to
 import torch.nn as nn
-import networkx as nx
 import matplotlib.pyplot as plt
 
 
+def get_device():
+    device = to.device('cuda' if to.cuda.is_available() else 'cpu')
+    print('running on', device)
+    return device
+
+
 def build_graph():
-    # 0->1, 0->2, 1-2
+    # edges: 0->1, 0->2, 1-2
     src = [0, 0, 1]
     tar = [1, 2, 2]
     graph = dgl.DGLGraph((src, tar))
@@ -17,12 +22,13 @@ def build_graph():
 
 
 def show_graph(graph):
+    import networkx as nx
     netx = graph.to_networkx()
     pos = nx.kamada_kawai_layout(netx)
     nx.draw(netx, pos, with_labels=True, font_size=9, node_color=[[.8, .8, .8]])
 
 
-def map_f(edges):  # = dgl.function.copy_u('h', 'm')
+def message_f(edges):  # = dgl.function.copy_u('h', 'm')
     return {'m': edges.src['h']}
 
 
@@ -37,14 +43,14 @@ class GCNLayer(nn.Module):
     def forward(self, graph, features):
         with graph.local_scope():
             graph.ndata['h'] = features
-            graph.update_all(map_f, reduce_f)
+            graph.update_all(message_f, reduce_f)
             h = graph.ndata['h']
             return h
 
 
-class Net(nn.Module):
+class GCN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(GCN, self).__init__()
         self.layer1 = GCNLayer()
 
     def forward(self, g, f):
@@ -54,9 +60,11 @@ class Net(nn.Module):
 
 
 if __name__ == "__main__":
-    net = Net()
+    gcn = GCN()
+    gcn.to(get_device())
+    print(gcn)
     g, f = build_graph()
     print(f)
-    print(net(g, f))
+    print(gcn(g, f))
     show_graph(g)
     plt.show()
